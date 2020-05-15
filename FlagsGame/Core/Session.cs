c1da9f2d83b7e77715c0c43a22689d2db61636e2
unknown <losmeus@gmail.com>
2020-05-15 21:34:06 +0200

    Added languages and main view

---
+++ FlagsGame/Core/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Core
+{
+    public class Session
+    {
+        string language;
+        ResourceDictionary dict;
+        Session()
+        {
+            this.dict = new ResourceDictionary();
+        }
+
+        public string Language { get => language; set => language = value; }
+    }
+}
